858aaaa7a5cee9fa9299179c80de9e03c4258ae9
datcosmi <ivan03idk@gmail.com>
2024-05-30 22:32:48 -0600

    si

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlumnosComponent } from './alumnos/alumnos.component';
+import { ConceptosComponent } from './conceptos/conceptos.component';
+import { DiagramaComponent } from './diagrama/diagrama.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnosComponent,
+    ConceptosComponent,
+    DiagramaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
